d3ea4aedd63568b71e47866abd68b1ec65ec6df6
Niels Liisberg <nli@system-method.com>
2025-04-23 17:56:22 +0200

    Added locust performace

---
+++ locust/echoPlusOne.py
@@ -0,0 +1,9 @@
+﻿# url  http://my_ibm_i:60800/.1/echoplusone?n=1&sleep=1
+# url  http://my_ibm_i:60800
+
+from locust import HttpUser, task
+
+class echoPlusOne (HttpUser):
+    @task
+    def echo_plus_one(self):
+        self.client.get("/echoplusone?n=1&sleep=0") 
