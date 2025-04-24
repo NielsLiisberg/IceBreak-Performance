baacd628b4c699c69c2188d16fc7427b60999f71
Niels Liisberg <nli@system-method.com>
2025-04-24 11:43:20 +0200

    iws comapred in locust

---
+++ locust/iwstemp.py
@@ -0,0 +1,8 @@
+﻿# url  http://MY_IBM_I:10019/web/services/CNVRTTMP/temperature/100
+
+from locust import HttpUser, task
+
+class indexhtml (HttpUser):
+    @task
+    def index_html(self):
+        self.client.get("/web/services/CNVRTTMP/temperature/100") 
